11df1781a9e20f10a621d8d66fe33d8a9c0889d4
Rajvir Kaur <rajvir.5jan@yahoo.com>
2019-11-19 18:33:48 -0500

    Add files via upload

---
+++ index.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="RajvirKaur_Lab4.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <asp:Label ID="Label1" runat="server" Text="Student Record"></asp:Label>
+            </tr>
+        </table>
+        <table>
+            
+            <tr>
+                <td>STUDENT ID</td>
+                <td>
+                    <asp:TextBox ID="studentID" runat="server"></asp:TextBox>
+                      <asp:RequiredFieldValidator ID="RequiredFieldValidator1" 
+                        runat="server" ControlToValidate="studentID" Display="Dynamic" ForeColor="red" ErrorMessage="Student ID is mandatory!!"></asp:RequiredFieldValidator>
+                    <asp:CompareValidator ID="CompareValidator1" runat="server" 
+                        ErrorMessage="The ID must be number!" Display="Dynamic"
+                        Operator="DataTypeCheck" Type="Integer" ForeColor="red" ControlToValidate="studentID" ></asp:CompareValidator>
+
+                  
+                </td>
+                 </tr> 
+            <tr>
+             <td> FIRST NAME:</td>
+                <td>
+                     <asp:TextBox ID="firstName" runat="server"></asp:TextBox>
+                    
+                </td>
+            </tr>
+               <tr>
+                 <td>LAST NAME</td>
+                   <td>
+                        <asp:TextBox ID="lastName" runat="server"></asp:TextBox>
+                       
+                   </td>
+               </tr>
+               <tr>
+                   <td>
+                       <asp:Button ID="addButton" runat="server" Text="Add Student" OnClick="addButton_Click" />
+                   </td>
+                   <td>
+                       <asp:Button ID="delButton" runat="server" Text="Delete Student Record" />
+                       <asp:Button ID="updateButton" runat="server" Text="Update Student Record" OnClick="updateButton_Click" />
+                       <asp:Button ID="getButton" runat="server" Text="Get existing Student IDs" OnClick="getButton_Click" />
+                   </td>
+                   <td>
+                       &nbsp;</td>
+               </tr>
+          
+        </table>
+        <asp:Label ID="message" runat="server" Text=""></asp:Label>
+        <asp:Label ID="stuID" runat="server" Text=""></asp:Label>
+        <asp:Label ID="lNAme" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RajvirKaur_Lab4
+{
+    public partial class index : System.Web.UI.Page
+    {
+        SqlCommand comm;
+
+        SqlConnection newConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\StudentDB.mdf;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+
+            comm = new SqlCommand("INSERT INTO Student(StudentID,FirstName,LastName)" + "VALUES (@stuID, @fName, @lName)", newConnection);
+            comm.Parameters.Add("@stuID", System.Data.SqlDbType.Int);
+            comm.Parameters["@stuID"].Value = studentID.Text;
+
+            comm.Parameters.Add("@fName", System.Data.SqlDbType.Char);
+            comm.Parameters["@fName"].Value = firstName.Text;
+
+            comm.Parameters.Add("@lName", System.Data.SqlDbType.Char);
+            comm.Parameters["@lName"].Value = lastName.Text;
+
+            newConnection.Open();
+            
+            comm.ExecuteNonQuery();
+            
+            
+            newConnection.Close();
+
+
+        }
+
+        protected void delButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void getButton_Click(object sender, EventArgs e)
+        {
+           
+                 comm = new SqlCommand("SELECT StudentID,LastName" + " FROM Student where StudentID = @stuID", newConnection);
+                int stuID;
+                if(!int.TryParse(studentID.Text, out stuID))
+            {
+                message.Text = "Please Enter the valid Student ID";
+            }
+            else
+            {
+
+                comm.Parameters.Add("@stuID", System.Data.SqlDbType.Int);
+                comm.Parameters["@stuID"].Value = stuID;
+                newConnection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                if(reader.Read())
+                {
+                    message.Text = "Student ID:" + " " +
+                        reader["StudentID"] + "<br/>" +
+                        "Last Name: " + " " + reader["LastName"] + "<br/>";
+                }
+                else
+                {
+                    message.Text = "There is no such Student Record  for ID" + " " +stuID;
+
+                }
+                reader.Close();
+                newConnection.Close();
+
+
+            }
+                
+            }
+
+        protected void updateButton_Click(object sender, EventArgs e)
+        {
+            comm = new SqlCommand("UPDATE Student SET FirstName = @fName, LastName = @lName WHERE StudentID = @stuID", newConnection);
+            comm.Parameters.Add("@stuID", System.Data.SqlDbType.Char);
+            comm.Parameters["@stuID"].Value = studentID.Text;
+
+            comm.Parameters.Add("@fName", System.Data.SqlDbType.Char);
+            comm.Parameters["@fName"].Value = firstName.Text;
+
+            comm.Parameters.Add("@lName", System.Data.SqlDbType.Char);
+            comm.Parameters["@lName"].Value = lastName.Text;
+
+            newConnection.Open();
+            comm.ExecuteNonQuery();
+            newConnection.Close();
+        }
+    }
+}
